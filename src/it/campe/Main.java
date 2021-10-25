d87cabfebf242ba67a63063f5b1fbde4f3e488d2
razmy <razmyslovichv@gmail.com>
2021-10-25 21:15:55 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -7,14 +7,14 @@ public class Main {
         int b = 2;
 
         System.out.print("два");
-        System.out.print(b);
+        // System.out.print(b);
         System.out.print(" плюс ");
-        System.out.print(" минус ");
-        System.out.print(a);
+        // System.out.print(" минус ");
+        // System.out.print(a);
         System.out.print("три");
         System.out.print(" равно ");
-        System.out.print(" будет ");
+        // System.out.print(" будет ");
         System.out.print("пять");
-        System.out.print(a + b);
+        // System.out.print(a + b);
     }
 }
